823e067b9f9ec246aef7dc63833dab08736aa3bd
Ozgur <ozgurozvaris@hotmail.com>
2021-07-12 09:36:30 +0300

    Mongo Basic CRUD operations finished

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,95 +1,102 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-exports.checkID = (req, res, next, val) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
+    res.status(200).json({
+      status: 'success',
+      results: tours.lenght,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID',
+      //message: "Invalid data sent",
     });
   }
-
-  next();
 };
 
-exports.checkBody = (req, res, next) => {
-  console.log('checkBody');
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
+exports.getTour = async (req, res) => {
+  try {
+    //console.log(req.params.id);
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(404).json({
       status: 'fail',
-      message: 'missing name or price',
+      message: 'Not Found',
     });
   }
-
-  next();
 };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    //const newTours = new Tour({})
+    //newTours.save();
+
+    //console.log(req.body);
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    //console.log(err);
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = { id: newId, ...req.body };
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-exports.updateTour = (req, res) => {
-  console.log(req.params);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
-};
-
-exports.deleteTour = (req, res) => {
-  console.log(req.params);
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-//console.log(app.get('env'));
-//console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    //console.log(con.connections);
+    console.log('DB connection successful! 💪');
+  });
+
+
+// const testTour = new Tour({
+//   name: 'The Forest Hiker',
+//   rating: 4.7,
+//   price: 497,
+// });
+
+// testTour.save().then((doc) => {
+//     console.log(doc);
+// }).catch(err => {
+//     console.log("ERROR ", err)
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
