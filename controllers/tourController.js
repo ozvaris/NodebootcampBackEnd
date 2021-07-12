54bd4aac98dbe12f1504dbae22aadaebbff005d8
Ozgur <ozgurozvaris@hotmail.com>
2021-07-12 11:42:09 +0300

    filtering sorting fields added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,11 +2,45 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUIKD QUERY
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const exculededFields = ['page', 'sort', 'limit', 'fields'];
+    exculededFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+    // let query = Tour.find(JSON.parse(queryStr), {
+    //   price: 1,
+    //   ratingsAverage: 2,
+    //   createdAt: 3,
+    // });
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //EXECUTE QUERY
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
-      results: tours.lenght,
+      results: tours.length,
       data: {
         tours,
       },
