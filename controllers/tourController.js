742dce508060c7ff3fe1a7afe05b9f22832cc87a
Ozgur <ozgurozvaris@hotmail.com>
2021-07-12 12:37:50 +0300

    alias route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,duration,difficulty,price,ratingsAverage';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     //BUIKD QUERY
