80ed7792116c11d00f4b6d9dbbf54271be39ec9e
Ozgur <ozgurozvaris@hotmail.com>
2021-07-29 15:19:37 +0300

    body-parser package added

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === 'development') {
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
 
@@ -86,8 +86,8 @@ app.use(
       'ratingsAverage',
       'maxGroupSize',
       'difficulty',
-      'price'
-    ]
+      'price',
+    ],
   })
 );
 
