0f5ec2f512267370553d105fb9603ecc2eedb48a
Ozgur <ozgurozvaris@hotmail.com>
2021-07-30 15:57:10 +0300

    webhook checkout place changed

--- app.js
+++ app.js
@@ -57,14 +57,14 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-app.use('/api', limiter);
-
 // Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
 app.post(
   '/webhook-checkout',
   express.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
+
+app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
