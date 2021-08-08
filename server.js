5e47005baee3ec575176a174e08042e403245ff7
Ozgur <ozgurozvaris@hotmail.com>
2021-08-08 10:23:06 +0300

    handshake added to runnin app port log

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ mongoose
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}... 🤝`);
 });
 
 process.on('unhandledRejection', (err) => {
